26316a3fec0549eabe785acca189034bbf5bbc46
Shlw <shlw_kevin@hotmail.com>
2016-12-16 14:19:31 +0800

    cube texture test

--- ModelGenerator/CubeGen.cpp
+++ ModelGenerator/CubeGen.cpp
@@ -18,30 +18,30 @@ const float po[2][4][3]={
 };
 
 FILE* os=fopen("cube.txt","w");
-void prt(int i,int j){
-    fprintf(os,"%.10f %.10f %.10f ",po[i][j][0],po[i][j][1],po[i][j][2]);
+void prt(int i,int j,float x,float y){
+    fprintf(os,"%.10f %.10f %.10f %.10f %.10f",po[i][j][0],po[i][j][1],po[i][j][2],x,y);
 }
 
 int main(){
     fprintf(os,"12\n");
 
-    prt(1,0); prt(1,1); prt(0,1); fprintf(os,"\n");
-    prt(1,0); prt(0,1); prt(0,0); fprintf(os,"\n");
+    prt(1,0,1,0); prt(1,1,0,0); prt(0,1,0,1); fprintf(os,"\n");
+    prt(1,0,1,0); prt(0,1,0,1); prt(0,0,1,1); fprintf(os,"\n");
 
-    prt(1,1); prt(1,2); prt(0,2); fprintf(os,"\n");
-    prt(1,1); prt(0,2); prt(0,1); fprintf(os,"\n");
+    prt(1,1,1,0); prt(1,2,0,0); prt(0,2,0,1); fprintf(os,"\n");
+    prt(1,1,1,0); prt(0,2,0,1); prt(0,1,1,1); fprintf(os,"\n");
 
-    prt(1,2); prt(1,3); prt(0,3); fprintf(os,"\n");
-    prt(1,2); prt(0,3); prt(0,2); fprintf(os,"\n");
+    prt(1,2,1,0); prt(1,3,0,0); prt(0,3,0,1); fprintf(os,"\n");
+    prt(1,2,1,0); prt(0,3,0,1); prt(0,2,1,1); fprintf(os,"\n");
 
-    prt(1,3); prt(1,0); prt(0,0); fprintf(os,"\n");
-    prt(1,3); prt(0,0); prt(0,3); fprintf(os,"\n");
+    prt(1,3,1,0); prt(1,0,0,0); prt(0,0,0,1); fprintf(os,"\n");
+    prt(1,3,1,0); prt(0,0,0,1); prt(0,3,1,1); fprintf(os,"\n");
 //top
-    prt(1,2); prt(1,1); prt(1,0); fprintf(os,"\n");
-    prt(1,2); prt(1,0); prt(1,3); fprintf(os,"\n");
+    prt(1,2,1,0); prt(1,1,0,0); prt(1,0,0,1); fprintf(os,"\n");
+    prt(1,2,1,0); prt(1,0,0,1); prt(1,3,1,1); fprintf(os,"\n");
 //button
-    prt(0,2); prt(0,0); prt(0,1); fprintf(os,"\n");
-    prt(0,2); prt(0,3); prt(0,0); fprintf(os,"\n");
+    prt(0,2,1,0); prt(0,0,0,0); prt(0,1,0,1); fprintf(os,"\n");
+    prt(0,2,1,0); prt(0,3,0,1); prt(0,0,1,1); fprintf(os,"\n");
 
     fclose(os);
 }
